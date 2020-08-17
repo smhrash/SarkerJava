cb78cccd849b15b401b56d64304da37241ddc02a
smhrash <smhrash@gmail.com>
2020-08-17 18:07:19 -0400

    update

--- src/practice/Demo.java
+++ src/practice/Demo.java
@@ -1,17 +1,25 @@
 package practice;
 
+
+
+
 public class Demo {
 
     public static void main(String[] args) {
 
-        int num = 12345;
 
-        int rev = 0;
-        while (num != 0) {
-            rev = rev*10 + num % 10;
-            num = num / 10;
+      long num = 1239876885;
 
-        }
+      long rev = 0;
+       while (num != 0){
+           rev = rev * 10 + num % 10;
+           num = num / 10;
+
+       }
+       int num1 = 3456;
         System.out.println(rev);
+       StringBuffer sb = new StringBuffer(String.valueOf(num1));
+        System.out.println(sb.reverse());
+
     }
 }
